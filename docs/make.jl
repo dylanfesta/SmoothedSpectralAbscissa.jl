99bfc8ec904904c924389902a2f64fa6752c007b
Dylan Festa <darksnail@gmail.com>
2021-02-20 22:27:21 +0100

    RIP travis

--- docs/make.jl
+++ docs/make.jl
@@ -2,17 +2,24 @@ push!(LOAD_PATH,"../src/")
 using Documenter, SmoothedSpectralAbscissa
 const SSA=SmoothedSpectralAbscissa
 
+DocMeta.setdocmeta!(SmoothedSpectralAbscissa, :DocTestSetup, :(using SmoothedSpectralAbscissa); recursive=true)
+
 makedocs(;
     modules=[SmoothedSpectralAbscissa],
-    format=Documenter.HTML(),
+    authors="Dylan Festa",
+    repo="https://github.com/dylanfesta/SmoothedSpectralAbscissa.jl/blob/{commit}{path}#{line}",
+    sitename="SmoothedSpectralAbscissa.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://dylanfesta.github.io/SmoothedSpectralAbscissa.jl",
+        assets=String[],
+    ),
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/dylanfesta/SmoothedSpectralAbscissa.jl/blob/{commit}{path}#L{line}",
-    sitename="SmoothedSpectralAbscissa.jl",
-    authors="Dylan Festa",
 )
 
 deploydocs(;
     repo="github.com/dylanfesta/SmoothedSpectralAbscissa.jl",
+    #devbranch="master",
 )
